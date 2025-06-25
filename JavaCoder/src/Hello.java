a424c61b4634c5178451f4e73a4dc703ee50b639
Sachin318-git <Sachinchavhan318@gmail.com>
2025-06-25 21:53:16 +0530

    Update Hello.java

--- JavaCoder/src/Hello.java
+++ JavaCoder/src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first program that i have commented on git repro");
 		System.out.println("Thanks");
+		System.out.println(2+5);
 	}
 
 }
