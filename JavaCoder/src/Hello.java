ac3ec01cae75a30c1803cf503a7a20703664ef24
admin <admin@DESKTOP-QK9928U>
2025-06-25 22:10:53 +0530

    Added login feature

--- JavaCoder/src/Hello.java
+++ JavaCoder/src/Hello.java
@@ -6,6 +6,11 @@ public class Hello {
 		System.out.println("This is my first program that i have commented on git repro");
 		System.out.println("Thanks");
 		System.out.println(2+5);
+		
+		String str="I Love INDIA";
+		
+		for(int i=str.length()-1;i>=0;i--)
+		System.out.println(str.charAt(i));
 	}
 
 }
